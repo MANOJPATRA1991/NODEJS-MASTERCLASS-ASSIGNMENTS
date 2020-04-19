266d563d043e56755cc5c6597b95c97013fe0aac
Manoj Kumar Patra <patra.manoj0@gmail.com>
2020-04-20 00:45:00 +0530

    Create generic function to create response

--- Assignment-1/callbacks.js
+++ Assignment-1/callbacks.js
@@ -4,77 +4,67 @@ let router = require('./router');
 
 let callbacks = {};
 
+function createResponse(res, statusCode, message) {
+	// Use the status code returned from the handler, or set the default status code to 200
+	statusCode = typeof statusCode == 'number' ? statusCode : 404;
+
+	// Return the response
+	res.setHeader('Content-Type', 'application/text');
+	res.writeHead(statusCode);
+	res.end(message);
+}
+
 // All the server logic for both HTTP and HTTPS server
 callbacks.unifiedServer = (req, res) => {
-    // Parse the url
-    let parsedUrl = url.parse(req.url, true);
+	// Parse the url
+	let parsedUrl = url.parse(req.url, true);
 
-    // Get the path
-    let path = parsedUrl.pathname;
-    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	// Get the path
+	let path = parsedUrl.pathname;
+	let trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    // Get the HTTP method
-    let method = req.method.toLowerCase();
+	// Get the HTTP method
+	let method = req.method.toLowerCase();
 
-    switch(method) {
-        case 'get':
-            let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : router.notFound;
+	switch (method) {
+		case 'get':
+			let chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : router.notFound;
 
-            // Route the request to the handler specified in the router
-            chosenHandler((statusCode) => {
+			// Route the request to the handler specified in the router
+			chosenHandler((statusCode) => {
+				createResponse(res, statusCode, `Hello`);
+			});
+			break;
 
-                // Use the status code returned from the handler, or set the default status code to 200
-                statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+		case 'post':
+			// Get the payload, if any
+			let decoder = new StringDecoder('utf-8');
 
-                // Return the response
-                res.setHeader('Content-Type', 'application/text');
-                res.writeHead(statusCode);
-                res.end(`Hello`);
-            });
-            break;
+			let buffer = '';
 
-        case 'post':
-            // Get the payload, if any
-            let decoder = new StringDecoder('utf-8');
-            
-            let buffer = '';
+			req.on('data', (data) => {
+				buffer += decoder.write(data);
+			});
 
-            req.on('data', data => {
-                buffer += decoder.write(data);
-            });
+			req.on('end', (data) => {
+				buffer += decoder.end();
 
-            req.on('end', data => {
-                buffer += decoder.end();
+				let chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : router.notFound;
 
-                let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : router.notFound;
+				// Route the request to the handler specified in the router
+				chosenHandler((statusCode) => {
+					const message = `Hello ${buffer !== '' ? buffer : 'user'}`;
+					createResponse(res, statusCode, message);
+				});
+			});
+			break;
 
-                // Route the request to the handler specified in the router
-                chosenHandler((statusCode) => {
-
-                    // Use the status code returned from the handler, or set the default status code to 200
-                    statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-                    // Return the response
-                    res.setHeader('Content-Type', 'application/text');
-                    res.writeHead(statusCode);
-                    res.end(`Hello ${buffer !== '' ? buffer : 'user'}`);
-                });
-            });
-            break;
-        
-        default:
-            // Route the request to the handler specified in the router
-            router.notFound((statusCode) => {
-
-                // Use the status code returned from the handler, or set the default status code to 200
-                statusCode = typeof(statusCode) == 'number' ? statusCode : 404;
-
-                // Return the response
-                res.setHeader('Content-Type', 'application/text');
-                res.writeHead(statusCode);
-                res.end(`Not found`);
-            });
-    }
+		default:
+			// Route the request to the handler specified in the router
+			router.notFound((statusCode) => {
+				createResponse(res, statusCode, `Not found`);
+			});
+	}
 };
 
 module.exports = callbacks;
